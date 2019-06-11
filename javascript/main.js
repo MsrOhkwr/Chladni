62b3ba44704330f1dfe101d925345fe8791eed56
MsrOhkwr <m.ohkawara@keio.jp>
2019-06-11 12:45:13 +0900

    fix typo

--- javascript/main.js
+++ javascript/main.js
@@ -15,7 +15,7 @@ function main()
 			tr.appendChild(td);
 			const img = document.createElement("img");
 			img.setAttribute("src", filename);
-			img.setAttribute("width", "25%");
+			img.setAttribute("width", "50%");
 			tr.appendChild(img);
 		}
 	}
