bc046c04d88a22057d4a06b504adf864824e6d88
MsrOhkwr <m.ohkawara@keio.jp>
2019-06-11 15:05:01 +0900

    upload

--- javascript/main.js
+++ javascript/main.js
@@ -17,7 +17,7 @@ function main()
 	text.setAttribute("y", "40");
 	text.setAttribute("font-size", "16");
 	text.setAttribute("font-family", "serif")
-	text.innerHTML = "<math><mi>m</mi>/<mi>n</mi></math>";
+	text.innerHTML = "m / n";
 	svg.appendChild(text);
 	for (let i = 1; i <= 8; i++)
 	{
