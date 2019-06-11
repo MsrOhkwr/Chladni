c75beb25943d7a3945d4f898325b545b1d9f4bd7
MsrOhkwr <m.ohkawara@keio.jp>
2019-06-11 13:24:59 +0900

    fix typo

--- javascript/main.js
+++ javascript/main.js
@@ -8,24 +8,28 @@ function main()
 	table.appendChild(tr);
 	for (let i = 0; i <= 8; i++)
 	{
+		const td = document.createElement("td");
+		tr.appendChild(td);
 		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 		svg.setAttribute("width", "64");
 		svg.setAttribute("height", "64");
-		tr.appendChild(svg);
+		td.appendChild(svg);
 	}
 	for (let i = 1; i <= 8; i++)
 	{
 		const tr = document.createElement("tr");
 		table.appendChild(tr);
+		const td = document.createElement("td");
+		tr.appendChild(td);
+		const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		svg.setAttribute("width", "64");
+		svg.setAttribute("height", "64");
+		td.appendChild(svg);
 		for (let j = 1; j <= 8; j++)
 		{
 			const filename = "./image/m" + i + "-n" + j + ext;
 			const td = document.createElement("td");
 			tr.appendChild(td);
-			const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-			svg.setAttribute("width", "64");
-			svg.setAttribute("height", "64");
-			tr.appendChild(svg);
 			const img = document.createElement("img");
 			img.setAttribute("src", filename);
 			img.setAttribute("width", "64");
