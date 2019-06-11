417c6c8cc9708a82917258f9da6019f0e6981492
MsrOhkwr <m.ohkawara@keio.jp>
2019-06-11 11:24:08 +0900

    change pattern

--- shader/fragmentShader.glsl
+++ shader/fragmentShader.glsl
@@ -8,8 +8,8 @@ void main(void)
 {
 	float x = screenPosition.x;
 	float y = screenPosition.y;
-	float m = 3.0;
-	float n = 8.0;
+	float m = 4.0;
+	float n = 2.0;
 	float z = abs(cos(n * PI * x) * cos(m * PI * y) - cos(m * PI * x) * cos(n * PI * y));
 	gl_FragColor = vec4(z, z, z, 1.0);
 }
