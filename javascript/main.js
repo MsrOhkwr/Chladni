b93413706914bb5d8ef0962c0063b6b7c94680c4
MsrOhkwr <m.ohkawara@keio.jp>
2019-06-11 15:12:15 +0900

    upload

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ function main()
 {
 	const title = document.createElement("h1");
 	title.innerHTML = "Chladni Patterns"
+	document.body,appendChild(title);
 	const table = document.createElement("table");
 	document.body.appendChild(table);
 
